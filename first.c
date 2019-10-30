9f6728e315f591bdbd1acb483707729a2bd61694
Subhraneil Das <57169966+subhraneildas@users.noreply.github.com>
2019-10-31 02:15:54 +0530

    Update first.c
    Added another function "countv" to count the number of vowels in the inputted string

--- first.c
+++ first.c
@@ -3,11 +3,15 @@
 int main ()
 {
     char palin(char[]);
+    int countv(char[]);
     char args[30];
     printf("Enter a string \n");
     gets(args);
     printf("Entered String is : \n");
     puts(args);
+    printf("The number of vowels in the given string is : \n");
+    countv(args);
+    printf("\n\n");
     palin(args);
     int i,x=0;
     for(i=0;args[i]!='\0';i++)
@@ -23,13 +27,12 @@ int main ()
     puts(args);
     return 0;
 }
-
-
 char palin(char str[30])
 {
     char revstr[30];
     int j,l=0,k=0;
     l=strlen(str);
+    printf("Length of inputted string : \n");
     printf("%d \n",l);
     j=l-1;
     while(j>=0)
@@ -39,6 +42,7 @@ char palin(char str[30])
         j--;
     }
     revstr[k]='\0';
+    printf("Reversed String is : \n");
     printf("%s \n", revstr);
     if (strcmpi(str,revstr)==0)
        {
@@ -49,4 +53,16 @@ char palin(char str[30])
         printf("The inputted string %s \n", str ); printf("is not a palindrome \n");
     }
         return str;
+}
+
+int countv(char x[30])
+{
+    int n=0,c=0;
+    for(n=0;x[n]!='\0';n++)
+    {
+        if(x[n]=='A'|| x[n]=='a'|| x[n]=='E'|| x[n]=='e' ||x[n]=='I'|| x[n]=='i'|| x[n]=='O'|| x[n]=='o'|| x[n]=='U'|| x[n]=='u')
+            c++;
+    }
+    printf("%d",c);
+    return c;
 }
